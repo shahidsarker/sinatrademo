aa4037873c8b2da02ba512ae77c6d297a5c32419
Shahid <shahid.sarker@gmail.com>
2019-12-05 13:24:06 -0500

    views and public folder

--- myapp.rb
+++ myapp.rb
@@ -4,3 +4,24 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+get '/time' do
+  time = "<%= Time.now %>"
+  erb time
+end
+
+get '/hello' do
+  mypage = '<div>
+  <h1>Hello!</h1>
+  <p>This is a sample!</p>
+  </div>
+  '
+  erb mypage
+end
+
+get '/hello/:name' do
+  # "Hello #{params['name']}!"
+  # puts params
+  erb :layout
+end
+
