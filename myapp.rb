53dc74b8254e5034531917d4b61ca7769e532207
Shahid <shahid.sarker@gmail.com>
2019-12-05 14:41:41 -0500

    basic setup

--- myapp.rb
+++ myapp.rb
@@ -3,25 +3,29 @@ require 'sinatra'
 
 get '/' do
   'Hello world!'
+  erb :home
 end
 
-get '/time' do
-  time = "<%= Time.now %>"
-  erb time
-end
-
-get '/hello' do
-  mypage = '<div>
-  <h1>Hello!</h1>
-  <p>This is a sample!</p>
-  </div>
-  '
-  erb mypage
-end
 
 get '/hello/:name' do
   # "Hello #{params['name']}!"
   # puts params
-  erb :layout
+  erb :hello, :layout => :home
 end
+
+# get '/time' do
+#   time = "<%= Time.now %>"
+#   erb time
+# end
+
+# get '/hello' do
+#   mypage = '<div>
+#   <h1>Hello!</h1>
+#   <p>This is a sample!</p>
+#   </div>
+#   '
+#   erb mypage
+# end
+
+
 
