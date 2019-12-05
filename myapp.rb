7c9a9ef8fe3b7ea96b0ae90c71989e386f53cb91
Shahid <shahid.sarker@gmail.com>
2019-12-05 15:15:26 -0500

    User creation form

--- myapp.rb
+++ myapp.rb
@@ -2,30 +2,33 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
-  erb :home
+  # 'Hello world!'
+  # erb :home
+  erb :home, :layout => :index
 end
 
+get '/user/new' do
+  erb :newuser
+end
 
-get '/hello/:name' do
+get '/user/:name' do
   # "Hello #{params['name']}!"
   # puts params
-  erb :hello, :layout => :home
+  @school = 'Year Up'
+  erb :user, :layout => :index
 end
 
-# get '/time' do
-#   time = "<%= Time.now %>"
-#   erb time
-# end
-
-# get '/hello' do
-#   mypage = '<div>
-#   <h1>Hello!</h1>
-#   <p>This is a sample!</p>
-#   </div>
-#   '
-#   erb mypage
-# end
 
 
+post '/user' do
 
+  puts params
+
+  redirect to('/user/'+params['user_name'])
+end
+
+
+# set up a page with a form
+# send a post request with the form
+# the form should have name, email, and fun fact
+# the post request should redirect you to a page where the form input is rendered
